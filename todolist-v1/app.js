0b6fca2ef68768be703d2a41695c46323549b5ff
Baker6Romeo <43591783+Baker6Romeo@users.noreply.github.com>
2022-09-06 18:48:20 -0700

    app.js boilerplate.

---
+++ todolist-v1/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", function(req, res) {
+  res.send("Hello");
+});
+
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Listening on port 3000.");
+});
