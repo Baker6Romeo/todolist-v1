1531e3b8fb69973b6237197dadb906e968158457
Baker6Romeo <43591783+Baker6Romeo@users.noreply.github.com>
2022-09-30 17:25:06 -0700

    Not the way to do it.

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.get("/", function(req, res) {
-  res.send("Hello");
+
+  var today = new Date();
+  if ( today.getDay() === 6 || today.getDay() === 0 ) {
+    res.sendFile(__dirname + "/weekend.html");
+  } else {
+    res.sendFile(__dirname + "/weekday.html");
+  }
 });
 
 
