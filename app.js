d279c91fda774af37b130ece1520fce670f11428
Baker6Romeo <43591783+Baker6Romeo@users.noreply.github.com>
2022-10-01 14:26:52 -0700

    Templating a list.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+let items = ["Buy Food","Cook Food","EatFood"];
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -17,11 +18,13 @@ app.get("/", (req, res) => {
   };
   let today = dayOfTheWeek.toLocaleDateString("en-US", options);
 
-  res.render('list', {today: today});
+  res.render('list', {list: items, today: today});
 });
 
 app.post("/", (req, res) => {
-  console.log(req.body.newItem);
+  let item = req.body.newItem;
+  items.push(item);
+  res.redirect("/")
 });
 
 // Depending on whether you're running local or hosted the port may be different
